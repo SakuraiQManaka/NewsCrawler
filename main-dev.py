f3ee600b023efa1b2aa37392fb73eb1f001dc84d
SakuraiQManaka <troywang@buaa.edu.cn>
2025-10-21 17:26:08 +0800

    V0.1updata

--- main-dev.py
+++ main-dev.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
+
 """
-Created on Thu Oct 16 22:32:58 2025
+新闻聚合与事件追踪系统
+Copyright (C) 2025 Troy Wang
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+"""
+Created on Tue Oct 21 17:25 2025
 
 @author: Troy
 """
